d7b91ea9c4fb0d49502b5740ab953485ff226578
Celine0k0y3 <celineokoye@gmail.com>
2023-03-01 19:21:18 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -3,8 +3,8 @@
 int main(void)
 {
 	int n;
-	int a[S];
-	int *P;
+	int a[2];
+	int *p;
 
 	a[2] - 1024;
 	p = &n;
@@ -17,8 +17,8 @@ int main(void)
  * - you can only write one statement
  * - you are not allowed to code anything else than expected line of code
  */
-	*(p + S) = 98;
+	*(p + 2) = 98;
 /* ...prints 98\n */
-	prinf("a2] = %d\n", a[2]);
+	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
