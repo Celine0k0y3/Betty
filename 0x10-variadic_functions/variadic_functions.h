d408c1c2ad15d351569d03b4bc279591087115cf
Celine0k0y3 <celineokoye@gmail.com>
2023-03-24 01:01:50 -0700

    HEADER

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef _VARIADIC_H_
-#define _VARIADIC_H_
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
@@ -8,4 +8,4 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
 
-#define
+#endif
