1e19f86002caaabfb94b43c021fc0bb60a708510
Celine0k0y3 <celineokoye@gmail.com>
2023-03-25 04:05:42 -0700

    MAINOPFUNCCALC

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -10,6 +10,12 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
+	op_add = "+";
+	op_sub = "-";
+	op_mul = "*";
+	op_div = "/";
+	op_mod = "%";
+
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
@@ -18,7 +24,7 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 
 		{NULL, NULL},
-	}
+	};
 	int i = 0;
 
 	while (ops[i].op != NULL && *(ops[i].op) != *s)
