3774d9773357d30f31d3063d307f92175482be70
Celine0k0y3 <celineokoye@gmail.com>
2023-04-02 15:18:10 -0700

    node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,10 +15,10 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	int len = 0;
+	unsigned int len = 0;
 
 	while (str[len])
-		++len;
+		len++;
 
 	new = malloc(sizeof(list_t));
 	if (!new)
